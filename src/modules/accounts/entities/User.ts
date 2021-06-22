f0548e624cf583cad03e7f3dad381d09e9e06855
pleaobraga <pleao.braga@gmail.com>
2021-06-22 11:16:38 -0300

    create account model

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,40 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
+import { v4 as uuidV4 } from "uuid"
+
+@Entity("user")
+class User {
+  @PrimaryColumn()
+  id?: string
+
+  @Column()
+  name: string
+
+  @Column()
+  username: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  driver_license: string
+
+  @Column()
+  isAdmin: boolean
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @CreateDateColumn()
+  updated_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
+
+export { User }
