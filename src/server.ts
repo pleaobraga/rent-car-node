6646a34b80855363db844fb40391b40ea2a71d15
pleaobraga <pleao.braga@gmail.com>
2021-06-21 21:39:51 -0300

    Add docker to the application

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express'
-import swagger from 'swagger-ui-express'
+import express from "express"
+import swagger from "swagger-ui-express"
 
-import { router } from './routes'
-import swaggerFile from './swagger.json'
+import { router } from "./routes"
+import swaggerFile from "./swagger.json"
 
 const app = express()
 
 app.use(express.json())
 
-app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile))
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile))
 
 app.use(router)
 
-app.listen(3333, () => console.log('server is running!'))
+app.listen(3333, () => console.log("server is running!"))
