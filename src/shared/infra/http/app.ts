2129e9218de058d56705f23254a40b8352bcc2fe
pleaobraga <pleao.braga@gmail.com>
2021-07-02 21:36:03 -0300

    Add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,13 +1,14 @@
+import cors from "cors"
 import express from "express"
 import swagger from "swagger-ui-express"
 import "dotenv/config"
 
 import "reflect-metadata"
+import upload from "../../../config/upload"
 import swaggerFile from "../../../swagger.json"
 import createConnection from "../../database"
-import "../../container"
 import { router } from "./routes"
-import upload from "../../../config/upload"
+import "../../container"
 
 createConnection()
 const app = express()
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile))
 
+app.use(cors())
 app.use(router)
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
